c89462470769cea260b973c52f1eb979a8b0c1f2
sahukari-omsai-chowdary <omsai.sahukari@gmail.com>
2021-04-21 21:03:07 +0530

    primary Test

---
+++ sampleExample/src/test/java/sampleExample/PrimaryTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package sampleExample;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Sahukari om sai chowary
+ *
+ */
+public class PrimaryTest {
+	@Test
+	public static void FirstTest() {
+		Assert.assertEquals(false, false);
+	}
+
+}
